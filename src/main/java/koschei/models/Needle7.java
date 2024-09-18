22f2694ca9ab88104cbe9fab755535a82fc95302
uncleZander <alex_zander69@mail.ru>
2024-09-18 16:46:15 +0300

    finish

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+public class Needle7 {
+
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + deth.toString();
+    }
+}
